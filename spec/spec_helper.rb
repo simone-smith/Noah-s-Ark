f34ea21ca41dd26dfa119a3234b250db68b53918
Simone <simonecaplin@gmail.com>
2018-06-07 10:47:44 +0100

    set up

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = NoahsArk
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.before(:each) do
+    require_relative './setup_test_database'
+  end
+
+end
