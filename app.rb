ccd8ebead121685df43a2537d5864ff7a65d965f
Simone <simonecaplin@gmail.com>
2018-06-07 12:20:30 +0100

    display most recently stowed animal

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require 'sinatra/base'
 
 class NoahsArk < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/ark' do
+    session[:female_animal] = params[:female_animal]
+    session[:male_animal] = params[:male_animal]
+    redirect('/result')
+  end
+
+  get '/result' do
+    @female_animal = session[:female_animal]
+    @male_animal = session[:male_animal]
     erb :result
   end
 
--- spec/features/record_spec.rb
+++ spec/features/record_spec.rb
@@ -1,9 +1,18 @@
-feature 'Animals board the ark' do
+feature 'Animals board the ark;' do
   scenario 'animals can board the ark and a confirmation message is displayed' do
     visit('/')
     fill_in('female_animal', with: 'Capybara_female')
     fill_in('male_animal', with: 'Capybara_male')
     click_button 'Board the ark'
     expect(page).to have_content 'Animals safely stowed in the ark!'
+  end
+
+  scenario 'check which animals are on board' do
+    visit('/')
+    fill_in('female_animal', with: 'Giraffe_female')
+    fill_in('male_animal', with: 'Giraffe_male')
+    click_button 'Board the ark'
+    expect(page).to have_content "Giraffe_female"
+    expect(page).to have_content "Giraffe_male"
   end
 end
